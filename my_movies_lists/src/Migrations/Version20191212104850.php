bd35207fc9288fd7b527f532c991192cffca3b5a
Emeric <emeric.charras@epitech.eu>
2019-12-12 12:25:07 +0100

    création de l'entité user

---
+++ my_movies_lists/src/Migrations/Version20191212104850.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191212104850 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD movies_lists_ids VARCHAR(255) NOT NULL, ADD is_banned TINYINT(1) NOT NULL, ADD is_verified TINYINT(1) NOT NULL, ADD created_at DATETIME NOT NULL, ADD modified_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP movies_lists_ids, DROP is_banned, DROP is_verified, DROP created_at, DROP modified_at');
+    }
+}
