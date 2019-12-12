3dcf8c06be51e3553acc2d5fba16021d1455a778
Emeric <emeric.charras@epitech.eu>
2019-12-12 16:56:48 +0100

    page lists created routes in annotation

--- my_movies_lists/src/Controller/ListsController.php
+++ my_movies_lists/src/Controller/ListsController.php
@@ -1,19 +1,19 @@
 <?php
-// src/Controller/HomeController.php
+// src/Controller/ListsController.php
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController extends AbstractController
+class ListsController extends AbstractController
 {
     /**
-    * @Route("/", name="home")
+    * @Route("/meslistes", name="list.index")
     * @return Response
     */
     public function index(): Response
     {
-        return $this->render('page/home.html.twig');
+        return $this->render('lists/index.html.twig');
     }
 }
